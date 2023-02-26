1a7745650cd782572088d60de668df03944ceb1f
גיא שטרצר <guystez@gmail.com>
2023-02-26 20:14:14 +0200

    finally

--- shopping/urls.py
+++ shopping/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views
-
+app_name = 'shopping'
 
 urlpatterns = [
     path("admin/", admin.site.urls),
