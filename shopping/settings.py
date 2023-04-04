9c9782cb4c3306cffda2e6ccef2d7b5d43328412
גיא שטרצר <guystez@gmail.com>
2023-04-04 19:51:04 +0300

    guy23new1

--- shopping/settings.py
+++ shopping/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "shopping.wsgi.application"
 
 DATABASES = {
     "default": {
-     'ENGINE': 'django.db.backends.sqlite3',
-     'NAME': BASE_DIR / 'db.sqlite3',
+     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     'NAME': 'guydatabase',
+     'USER': 'guysterzer',
+     'PASSWORD': 'ETMt1viICp8HH8prTEb5gpioynuNAlcv',
+     'HOST': 'dpg-cf7bbqmn6mplrj31fh40-a',
+     'PORT': '5432',
     }
 }
     
